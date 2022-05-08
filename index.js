dbbffc73db64ae929af800fbd48c7d4b89e1f70d
noelensith <mbnoelen@gmail.com>
2022-05-07 17:16:27 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+
+function submitData(name, email) {
+    const bodyData = {name, email}
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(bodyData)
+      } )
+      .then(function(resp) {
+        return resp.json()
+      } )
+      .then(function(object) {
+        document.body.innerHTML = object["id"]
+      })
+      .catch(function (error) {
+        document.body.innerHTML = error.message
+      })
+  }
